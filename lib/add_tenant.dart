4ac2b76edc6899f0855c56b21b59940cd2077a10
Ssentongo-Mansoor <ssentongomansoor@gmail.com>
2021-03-08 15:48:54 +0300

    Add tenant

--- lib/add_tenant.dart
+++ lib/add_tenant.dart
@@ -1,6 +1,7 @@
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:waka/manage_tenants.dart';
 
 class AddTenant extends StatefulWidget {
   @override
@@ -27,6 +28,277 @@ class _AddTenantState extends State<AddTenant> {
           ],
         ),
         body: Center(
-            child: Container(width: 320, height: 500, color: Colors.black)));
+            child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+            // padding: EdgeInsets.all(20.0),
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "First Name",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "First Name",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Last Name",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Last Name",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Other Names",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Other Names",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Gender",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Select Gender",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Nantional ID Number",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "NIN Number",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Other Identification",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Other ID No.",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Email Address",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Email Address",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Phone Number",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Phone Number",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Occupation",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Occupation",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Comapny",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                        hintText: "Company",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              Container(
+                width: 300,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child:
+                    /*ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ManageTenants()),
+                    );
+                  },
+                  child: Text('Add'),
+                ),*/
+                    FlatButton(
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.red),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ManageTenants()),
+                    );
+                  },
+                  child: Text(
+                    "Add",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )));
   }
 }
